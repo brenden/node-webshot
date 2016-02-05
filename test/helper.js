ebdf0addba8f8cfebf150db779b6d2231c9b2613
Brenden Kokoszka <brenden.kokoszka@gmail.com>
2016-02-04 23:04:39 -0800

    Separating out test helpers

---
+++ test/helper.js
@@ -0,0 +1,20 @@
+exports.fixtures = [
+  {
+    path: 'file://' + __dirname + '/fixtures/1.html'
+  , width: 1024
+  , height: 999
+  }
+, {
+    path: 'file://' + __dirname + '/fixtures/2.html'
+  , width: 1024
+  , height: 1000
+  }
+, {
+    path: 'file://' + __dirname + '/fixtures/3.html'
+  , width: 300
+  , height: 250
+  }
+];
+
+exports.pngOutput = __dirname + '/test.png';
+exports.pdfOutput = __dirname + '/test.pdf';
